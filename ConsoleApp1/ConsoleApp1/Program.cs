521e9e9040bd42e71966ab127d0c0fb722696827
Anastasia538 <asrn@simcorp.com>
2026-08-20 22:51:41 +0200

    Revert "Create a pull request to master branch from feature-branch-1."

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,7 +9,6 @@ namespace HelloWorldConsole
             Console.WriteLine("Hello world!");
             Console.WriteLine("Feature branch 1 - development 2");
 
-            Console.WriteLine("Feature branch 1 - development 1");
         }
     }
 }
