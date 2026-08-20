80b54ed83b8c7d3719f40c3c1d8dd01df41a8ece
Anastasiia Rumyna <Anastasiia.Rumyna@simcorp.com>
2026-08-20 21:42:05 +0200

    Added message for console output

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
